b719f8ee11b49da64b2ebfb72cf5e647ea4ea112
oladele.leslie <grzegorz.oladele@gmail.com>
2022-04-07 16:53:27 +0200

    refactoring - java.runtime.version=11

--- src/main/java/pl/envelo/restapi/controller/ChuckController.java
+++ src/main/java/pl/envelo/restapi/controller/ChuckController.java
@@ -1,7 +1,6 @@
 package pl.envelo.restapi.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import pl.envelo.restapi.service.ChuckService;
 
 @RestController
 @RequestMapping("/chuck")
-@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class ChuckController {
 
