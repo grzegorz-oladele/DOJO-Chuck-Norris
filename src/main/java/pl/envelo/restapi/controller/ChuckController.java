03c4934493794e2b1e8c57848c79e9163ae8a1b9
oladele.leslie <grzegorz.oladele@gmail.com>
2022-04-07 09:40:36 +0200

    Cross Origin

--- src/main/java/pl/envelo/restapi/controller/ChuckController.java
+++ src/main/java/pl/envelo/restapi/controller/ChuckController.java
@@ -1,6 +1,7 @@
 package pl.envelo.restapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import pl.envelo.restapi.service.ChuckService;
 
 @RestController
 @RequestMapping("/chuck")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class ChuckController {
 
