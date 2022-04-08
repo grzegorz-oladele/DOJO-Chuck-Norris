00515b72efd119faeb2f2033d18434128293471d
oladele.leslie <grzegorz.oladele@gmail.com>
2022-04-08 09:20:29 +0200

    refactoring vol.6

--- src/main/java/pl/envelo/restapi/controller/ChuckController.java
+++ src/main/java/pl/envelo/restapi/controller/ChuckController.java
@@ -1,6 +1,7 @@
 package pl.envelo.restapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import pl.envelo.restapi.service.ChuckService;
 @RestController
 @RequestMapping("/chuck")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://dojo-chuck-joke.herokuapp.com/chuck")
 public class ChuckController {
 
     private final ChuckService service;
